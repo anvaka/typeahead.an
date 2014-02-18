5b2d7ac919a77c9b9c051ac72b1c8767b06d077c
anvaka <anvaka@gmail.com>
2014-02-17 19:43:00 -0800

    This is what I'm trying to do here

---
+++ demo/index.js
@@ -0,0 +1,16 @@
+module.exports = function ($scope, $http) {
+  $scope.getLocation = function(val) {
+      return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+          address: val,
+          sensor: false
+        }
+      }).then(function(res){
+        var addresses = [];
+        angular.forEach(res.data.results, function(item){
+          addresses.push(item.formatted_address);
+        });
+        return addresses;
+      });
+    };
+};
